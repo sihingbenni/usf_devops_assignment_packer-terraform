7fe00e1bc969680e9792c9b4ee2fb68ac115dffb
Benjamin Puhani <bpuhani@usfca.edu>
2025-03-26 16:52:20 -0700

    MOD: reverted assignment_9 changes

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -38,42 +38,6 @@ resource "aws_security_group" "private-ssh" {
     security_groups = [aws_security_group.bastion-allow-ssh.id]
   }
 
-  # Ports öffnen
-  ingress {
-    from_port   = 2377
-    to_port     = 2377
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 7946
-    to_port     = 7946
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 7946
-    to_port     = 7946
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4789
-    to_port     = 4789
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4789
-    to_port     = 4789
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "private-ssh"
   }
