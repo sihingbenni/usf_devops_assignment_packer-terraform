0664f7275f929c2a22e964c51dbb708eb120d070
Benjamin Puhani <bpuhani@usfca.edu>
2025-03-26 22:56:30 -0700

    MOD: moved instances into seperate files

--- terraform/public_instances.tf
+++ terraform/public_instances.tf
@@ -1,5 +1,6 @@
+# The Bastion Host
 resource "aws_instance" "bastion" {
-  ami                    = var.ami_id
+  ami                    = var.linux_ami_id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.bastion-allow-ssh.id]
